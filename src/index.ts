261618a9a814827a2b88f947098fd77ceda09c3e
jc <root@sha3.dev>
2023-08-30 00:42:19 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 /* eslint-disable import/first */
 
 export { default as Storage } from "./lib/storage";
-export { default as Firestore } from "./lib/firestore";
+export { default as Firestore, type FirestoreOptions } from "./lib/firestore";
